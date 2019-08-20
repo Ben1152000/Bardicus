ecd2ec36aaecdb40a5492b51335c869519d6481b
Ben1152000 <benjamindarnell00@gmail.com>
2019-08-20 16:15:09 -0700

    bring remote repo up-to-date

--- download.py
+++ download.py
@@ -5,7 +5,8 @@ from pytube import YouTube
 
 URL = "https://www.youtube.com/watch?v={}"
 SHORT_URL = "https://youtu.be/{}"
-PATH = "./dir/"
+PATH = "./music/"
+KEYWORDS = "keywords.json"
 
 def ask(query):
     answer = None
@@ -17,7 +18,8 @@ def ask(query):
 
 def downloadVideo(videourl, path, filename):
     yt = YouTube(videourl)
-    title = f"{filename} ({int(yt.length) // 60}:{int(yt.length) % 60}) - {yt.title}"
+    data = {"title": yt.title, "description": yt.description, "length": yt.length}
+    title = f"{filename} ({int(yt.length) // 60}:{(int(yt.length) % 60):02d}) - {yt.title}"
     description = f"\n\t{yt.description[:100]}{'...' if len(yt.description) > 100 else ''}"
     print(f"{title}{description if len(description.strip()) > 0 else ''}")
     if (int(yt.length) < 600 or ask(f"\tVideo length is over 10 minutes, are you sure you want to download? ")):
@@ -25,14 +27,28 @@ def downloadVideo(videourl, path, filename):
         if not os.path.exists(path):
             os.makedirs(path)
         yt.download(path, filename=filename)
+        return data
 
-# downloads a youtube video to a file located in ./dir/
+# downloads a youtube video to a file located in PATH
 def download(code):
     code = code.strip()
     if (not len(code) == 11):
         print(f"Invalid Youtube Code: <{code}>"); return
     if (not os.path.isfile(f"{PATH}{code}.mp3")):
-        downloadVideo(SHORT_URL.format(code), PATH, code)
+        data = downloadVideo(SHORT_URL.format(code), PATH, code)
         os.system(f"ffmpeg -n -loglevel panic -i {PATH}{code}.mp4 {PATH}{code}.mp3")
         if os.path.exists(f"{PATH}{code}.mp4"): os.remove(f"{PATH}{code}.mp4")
+        return data
 
+import json
+
+with open(KEYWORDS) as keywordsFile:
+    keywords = json.loads(keywordsFile.read())
+    for code in keywords:
+        if (len(code.strip()) > 0):
+            data = download(code.strip())
+            if data != None:
+                keywords[code] = (data)
+                if "keywords" not in keywords[code]:
+                    keywords[code]["keywords"] = []
+    json.dump(keywords, open(KEYWORDS, 'w'), indent=3)
--- list.py
+++
@@ -1,9 +0,0 @@
-
-import download
-
-with open("list.txt") as listFile:
-    line = listFile.readline()
-    while (line):
-        if (len(line.strip()) > 0):
-            download.download(line.strip())
-        line = listFile.readline()
